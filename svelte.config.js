c8752771cc96ca9184d520c3976e8ae32d7d0303
anthonyprime202 <anthonyprime202@gmail.com>
2025-04-16 05:00:24 +0530

    Automatic switch adapters for prod and dev environment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import auto from "@sveltejs/adapter-auto";
+import cloudflare from "@sveltejs/adapter-cloudflare";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+const isCloudflare = process.env.CF_PAGES === "1" || process.env.ADAPTER === "cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,8 +14,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: isCloudflare ? cloudflare() : auto(),
+	},
 };
 
 export default config;

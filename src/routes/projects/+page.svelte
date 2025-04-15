d8b039a153a4c88cc03220f8da14af885062d779
anthonyprime202 <anthonyprime202@gmail.com>
2025-04-16 05:01:10 +0530

    Added placeholder content

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { fly } from "svelte/transition";
+
+	let delayShow = $state(false);
+
+	onMount(() => {
+		setTimeout(() => {
+			delayShow = true;
+		}, 100);
+	});
+</script>
+
+{#if delayShow}
+	<main>
+		<h1 transition:fly={{ y: 20, duration: 1000 }}>WORK IN PROGRESS ⚠️</h1>
+	</main>
+{/if}
+
+<style>
+	main {
+		height: 100vh;
+		width: 100%;
+		display: grid;
+		place-items: center;
+	}
+</style>
